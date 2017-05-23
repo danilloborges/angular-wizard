ec28c92293c5cb5c0cb22398abdd3c52270c2039
danillo <danillo@novaxs.com.br>
2017-05-23 16:21:40 -0300

    Better example.

--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,16 @@
 angular.module('your-app', ['mgo-angular-wizard'])
 .controller('testController',
-    function($scope){
+    function($scope, $log){
         $scope.subtitle = "Meu subtitle";
         $scope.resume = "Passo final";
         $scope.changeSubtitle = function () {
             $scope.subtitle = "Meu new subtitle";
         };
+        $scope.log = function(message){
+            $log.info(message);
+        };
+        $scope.$on('wizard:stepChanged', function(event, change) {
+            $log.info(change);
+        });
     }
 );
